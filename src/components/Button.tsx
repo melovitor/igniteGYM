0a98432d04c9bf7400207af6072636e9821bf065
melovitor <vitormelobatista@hotmail.com>
2023-05-22 13:14:28 -0300

    WIP - Finalizando SignIn e Criando SignUp.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,25 +1,28 @@
 import {Button as NativeBaseButton, IButtonProps, Text} from 'native-base'
 
 type Props = IButtonProps & {
-    title: string
+    title: string;
+    variant?: 'solid'| 'outline'
 }
 
-export function Button({ title, ...rest}: Props){
+export function Button({ title, variant, ...rest}: Props){
     return (
         <NativeBaseButton 
             w='full'
             h={14}
-            bg='green.700'
+            bg={variant === 'outline' ? 'transparent' :'green.700'}
+            borderWidth={variant === 'outline' ? 1 : 0}
+            borderColor='green.500'
             rounded='lg'
 
             _pressed={{
-                bg: 'green.500'
+                bg: variant === 'outline' ? 'gray.500' :'green.500'
             }}
 
             {...rest}
         >
             <Text 
-                color='white' 
+                color={variant === 'outline' ? 'green.500' :'white' }
                 fontFamily='heading' 
                 fontSize='sm'
             >
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -4,7 +4,7 @@ import BackgroundImg from '@assets/background.png'
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
-export function SignIn(){
+export function SignUp(){
     return(
         <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>
             <VStack flex={1} bg='gray.700' px={10} pb={16} >
@@ -22,8 +22,11 @@ export function SignIn(){
                 </Center>
                 <Center>
                     <Heading color='gray.100' mb={6} fontFamily='heading'>
-                        Acesse sua conta
+                        Crie sua conta
                     </Heading>
+                    <Input 
+                        placeholder='Nome' 
+                    />
                     <Input 
                         placeholder='E-mail' 
                         keyboardType='email-address'
@@ -35,17 +38,12 @@ export function SignIn(){
                         secureTextEntry
                     />
                     <Button
-                        title='Acessar'
+                        title='Criar e acessar'
                     />
                 </Center>
-                <Center mt={24}>
-                    <Text color='gray.100' fontSize='sm' mb={3} fontFamily='body'>
-                        Ainda não tem acesso?
-                    </Text>
-                    <Button
-                        title='Criar conta' variant='outline'
-                    />
-                </Center>
+                <Button
+                    title='Voltar para o login' variant='outline' mt={24}
+                />
             </VStack>
         </ScrollView>
         
