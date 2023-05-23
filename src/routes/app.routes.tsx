f583e29414ebb0264aaad7bb6390edcec494fb4c
melovitor <vitormelobatista@hotmail.com>
2023-05-23 15:14:21 -0300

    WIP - Tipando as rotas do App.

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,10 +1,19 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
 import { Exercise } from '@screens/Exercise'
 import { History } from '@screens/History'
 import { Home } from '@screens/Home'
 import { Profile } from '@screens/Profile'
 
-const {Navigator, Screen} = createBottomTabNavigator()
+type AppRouts = {
+    home: undefined
+    history:undefined
+    profile:undefined
+    exercise:undefined
+}
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRouts>
+
+const {Navigator, Screen} = createBottomTabNavigator<AppRouts>()
 
 
 export function AppRoutes(){
