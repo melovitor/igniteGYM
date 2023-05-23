18192e0964fb804dec52bf5baad3e03d6fb1f19a
melovitor <vitormelobatista@hotmail.com>
2023-05-23 14:22:09 -0300

    WIP -  Aplicando navegação de autenticação.

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,13 +1,19 @@
 import { VStack, Image, Center, Text, Heading, ScrollView} from 'native-base';
+import {AuthNavigatorRoutesProps} from '@routes/auth.routes'
+import {useNavigation} from '@react-navigation/native'
 import LogoSVG from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
-import { Loading } from '@components/Loading';
 
 export function SignIn(){
-    return(
-        
+    const navigation = useNavigation<AuthNavigatorRoutesProps>()
+
+    function handleNewAccount(){
+        navigation.navigate('signUp')
+    }
+
+    return(        
         <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>
             <VStack flex={1} bg='gray.700' px={10} pb={16} >
                 <Image
@@ -46,7 +52,7 @@ export function SignIn(){
                         Ainda não tem acesso?
                     </Text>
                     <Button
-                        title='Criar conta' variant='outline'
+                        title='Criar conta' variant='outline' onPress={handleNewAccount}
                     />
                 </Center>
             </VStack>
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,10 +1,18 @@
 import { VStack, Image, Center, Text, Heading, ScrollView} from 'native-base';
+import {useNavigation} from '@react-navigation/native'
 import LogoSVG from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 export function SignUp(){
+
+    const navigation = useNavigation()
+
+    function handleGoBack(){
+        navigation.goBack()
+    }
+
     return(
         <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>
             <VStack flex={1} bg='gray.700' px={10} pb={16} >
@@ -42,7 +50,7 @@ export function SignUp(){
                     />
                 </Center>
                 <Button
-                    title='Voltar para o login' variant='outline' mt={24}
+                    title='Voltar para o login' variant='outline' mt={24} onPress={handleGoBack}
                 />
             </VStack>
         </ScrollView>
