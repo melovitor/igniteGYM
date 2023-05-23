5b52dc03369600a88159cae34da48c2e8ba4f44f
melovitor <vitormelobatista@hotmail.com>
2023-05-23 16:39:58 -0300

    WIP - Alterando os ícones da Bottom Navigator.

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,8 +1,12 @@
 import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
+import { useTheme } from 'native-base'
 import { Exercise } from '@screens/Exercise'
 import { History } from '@screens/History'
 import { Home } from '@screens/Home'
 import { Profile } from '@screens/Profile'
+import HomeSvg from '@assets/home.svg'
+import HistorySvg from '@assets/history.svg'
+import ProfileSvg from '@assets/profile.svg'
 
 type AppRouts = {
     home: undefined
@@ -17,6 +21,10 @@ const {Navigator, Screen} = createBottomTabNavigator<AppRouts>()
 
 
 export function AppRoutes(){
+
+    const {sizes} = useTheme()
+    const iconSize = sizes[6]
+    
     return(
         <Navigator screenOptions={{
                 headerShown: false,
@@ -26,14 +34,29 @@ export function AppRoutes(){
             <Screen
                 name='home'
                 component={Home}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <HomeSvg fill={color} width={iconSize} height={iconSize}/>
+                    )
+                }}
             />
             <Screen
                 name='history'
                 component={History}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <HistorySvg fill={color} width={iconSize} height={iconSize}/>
+                    )
+                }}
             />
             <Screen
                 name='profile'
                 component={Profile}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <ProfileSvg fill={color} width={iconSize} height={iconSize}/>
+                    )
+                }}
             />
             <Screen
                 name='exercise'
